2e241341d28f574eed04a6874f4b0381e6a9a721
Adrien Audebert <aaudeber@student.42perpignan.fr>
2024-01-12 16:36:07 +0100

    done

--- ex02/srcs/Account.cpp
+++ ex02/srcs/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: motoko <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 15:13:19 by motoko            #+#    #+#             */
-/*   Updated: 2024/01/11 19:04:42 by motoko           ###   ########.fr       */
+/*   Updated: 2024/01/12 16:34:52 by motoko           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 #include "iostream"
 
 int	Account::_nbAccounts = 0;
-/*
-int	totalAmount = 0;
-int	nbDeposits = 0;
-int	totalNbDeposits = 0;
-*/
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
 Account::Account(void) {
 	std::cout << "Contructor Account!" << std::endl;
@@ -27,12 +25,7 @@ Account::Account(void) {
 Account::Account(int initial_deposit) {
 	_amount = initial_deposit;
 	_accountIndex = _nbAccounts++;
-	/*
-	_nbAccounts = nbAccounts++;
-	_totalAmount = totalAmount;
-	_totalNbDeposits = totalNbDeposits;
-	_totalNbWithdrawals = totalNbWithdrawals;
-	*/
+	_totalAmount += _amount;
 	std::cout << "index:" << _accountIndex << ";" << "amount:" << _amount << ";" << "created" << std::endl;
 }
 
@@ -40,15 +33,39 @@ Account::~Account(void) {
 	std::cout << "index:" << _accountIndex << ";" << "amount:" << _amount << ";" << "closed" << std::endl;
 }
 
-void	Account::displayStatus(void) const
-{
+void	Account::displayStatus(void) const {
 	std::cout << "index:" << _accountIndex << ";" << "amount:" << _amount << ";" << "deposits:" << _nbDeposits << ";" << "withdrawals:" << _nbWithdrawals << std::endl;
 }
 
-void		Account::displayAccountsInfos(void) {
-	std::cout << "totot" << std::endl;
-	//[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
-	//std::cout << "accounts:" << nbAccounts << ";" << "total:" << totalAmount << ";" << "deposits:" << nbDeposits << ";" << "withdrawals:" << totalNbDeposits << std::endl;
+void	Account::displayAccountsInfos(void) {
+	std::cout << "accounts:" << _nbAccounts << ";" << "total:" << _totalAmount << ";" << "deposits:" << _totalNbDeposits << ";" << "withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	int	p_amount = _amount;
+	_amount += deposit;
+	_totalAmount += deposit;
+	_nbDeposits++;
+	_totalNbDeposits++;
+	std::cout << "index:" << _accountIndex << ";" << "p_amount:" << p_amount << ";" << "deposit:" << deposit << ";" << "amount:" << _amount << ";" << "nb_deposits:" << _nbDeposits << std::endl;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	int	p_amount = _amount;
+	if (withdrawal > _amount)
+		std::cout << "index:" << _accountIndex << ";" << "p_amount:" << p_amount << ";" << "withdrawal:" << "refused" << std::endl;
+	else
+	{
+
+		_amount -= withdrawal;
+		_totalAmount -= withdrawal;
+		_nbWithdrawals++;
+		_totalNbWithdrawals++;
+		std::cout << "index:" << _accountIndex << ";" << "p_amount:" << p_amount << ";" << "withdrawal:" << withdrawal << ";" << "amount:" << _amount << ";" << "nb_withdrawals:" << _nbWithdrawals << std::endl;
+	}
+	return (true);
 }
 
 /*
@@ -70,18 +87,6 @@ static int	getNbDeposits(void)
 static int	getNbWithdrawals(void)
 {
 	return (_totalNbWithdrawals);
-}
-
-void	makeDeposit(int deposit)
-{
-	_amount += deposit;
-	_nbDeposits++;
-}
-
-bool	makeWithdrawal(int withdrawal)
-{
-	_amount -= withdrawal;
-	_nbWithdrawals++;
 }
 
 int		checkAmount(void) const
--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: motoko <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 14:36:54 by motoko            #+#    #+#             */
-/*   Updated: 2024/01/11 19:01:02 by motoko           ###   ########.fr       */
+/*   Updated: 2024/01/12 16:18:36 by motoko           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,27 @@ int	main(void)
 {
 	Account	t(42);
 	Account	y(54);
+	Account	z(60);
+
+	Account::displayAccountsInfos();
 
 	t.displayStatus();
 	y.displayStatus();
+	z.displayStatus();
+
+	t.makeDeposit(5);
+	y.makeDeposit(765);
 
 	Account::displayAccountsInfos();
+
+	t.makeWithdrawal(100);
+	y.makeWithdrawal(10);
+
+	Account::displayAccountsInfos();
+
+	t.displayStatus();
+	y.displayStatus();
+	z.displayStatus();
+
 	return (0);
 }
